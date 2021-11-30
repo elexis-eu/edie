5d6aa27f429249f77c0b8690c3ecdc72f5c1edde
John P. McCrae <john@mccr.ae>
2021-11-30 11:47:00 +0000

    Complete model and add tests

---
+++ test/test_model.py
@@ -0,0 +1,77 @@
+import sys
+import unittest
+import json
+
+from edie.model import *
+
+
+class TestParsing(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def testReadAbout(self):
+        data = json.loads("""{
+  "release": "PUBLIC",
+  "sourceLanguage": "en",
+  "targetLanguage": [
+    "en",
+    "de"
+  ],
+  "genre": [
+    "gen"
+  ],
+  "license": "https://creativecommons.org/licenses/by/4.0/",
+  "title": "The Human-Readable Name of this resource",
+  "creator": [
+    {
+      "name": "Institute of This Resource",
+      "email": "contact@institute.com"
+    }
+  ],
+  "publisher": [
+    {
+      "name": "Publishing Company"
+    }
+  ]
+}""")
+        result = Metadata(data)
+        self.assertEqual(len(result.errors), 0)
+
+    def testEntry(self):
+        data = json.loads("""{
+    "release": "PUBLIC",
+    "lemma": "string",
+    "language": "en",
+    "id": "string",
+    "partOfSpeech": [
+      "ADJ"
+    ],
+    "formats": [
+      "tei"
+    ]
+  }""")
+        result = Entry(data)
+        self.assertEqual(len(result.errors), 0)
+
+    def testJsonEntry(self):
+        data = json.loads("""{
+  "@context": "http://lexinfo.net/jsonld/3.0/content.json",
+  "@type": "Word",
+  "language": "en",
+  "partOfSpeech": "commonNoun",
+  "canonicalForm": {
+    "writtenRep": "example"
+  },
+  "senses": [
+    {
+      "definition": "An example OntoLex Entry"
+    }
+  ]
+}""")
+        result = JsonEntry(data)
+        self.assertEqual(len(result.errors), 0)
+
